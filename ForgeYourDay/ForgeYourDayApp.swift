4e0fb4b1312cc533ea376d6a889d168100e11868
taenam <tkim356@wisc.edu>
2025-06-30 23:26:29 +0900

    Initial Commit

---
+++ ForgeYourDay/ForgeYourDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeYourDayApp.swift
+//  ForgeYourDay
+//
+//  Created by 맥북 on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ForgeYourDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
